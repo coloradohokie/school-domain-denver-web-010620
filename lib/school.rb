f0de5a0bebe0a7582f5631efae37073210612a68
Michael Newman <coloradohokie@gmail.com>
2020-01-10 10:24:59 -0700

    initial commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
-# code here!
+require 'pry'
+
+class School
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def name
+		@name
+	end
+
+	def roster
+		@roster
+	end
+
+	def add_student(student_name,grade)
+		if !@roster[grade]
+			@roster[grade] = []
+		end
+		@roster[grade] << student_name
+	end
+
+	def grade(grade_num)
+		@roster[grade_num]
+	end
+
+	def sort
+		@roster.each do |key, value|
+			roster[key] = value.sort
+		end		
+	end
+
+
+
+end
+
+school =School.new("Bayside High School")
+
+school.add_student("Zach Morris",9)
+school.add_student("Homer Simpson", 2)
+school.add_student("AJ Slater", 9)
+
+p school.sort
